6b8d60ff0860be58af1c888860a612d8079d6bca
tangtang <a5511572@gmail.com>
2024-07-29 19:48:59 +0800

    Change the dir of make.bat and Makefile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,9 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-# -- Options for HTML output
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 
